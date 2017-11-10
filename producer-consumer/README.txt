1c8a8f5acef5930209a47ac294c6c0399bb73a59
Brijesh Kachalia <bkachalia@ets.org>
2017-11-10 14:49:54 -0500

    intial checkin

---
+++ producer-consumer/README.txt
@@ -0,0 +1,6 @@
+Producer-Consumer
+-------------------------------------------------
+
+1) Import Producer and Consumer in eclipse environment as Maven Project <br>
+2) Run EmployeeProducerApplication and on browser hit http://localhost:8080/employee <br>
+3) Run EmployeeConsumerApplication, check console to see the same data as in above URL.
---
+++ producer-consumer/employee-consumer/src/main/java/com/consumer/EmployeeConsumerApplication.java
@@ -0,0 +1,30 @@
+package com.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.consumer.controllers.ConsumerControllerClient;
+
+@SpringBootApplication
+public class EmployeeConsumerApplication {
+
+	public static void main(String[] args) {
+		
+		
+		ApplicationContext ctx = SpringApplication.run(
+				EmployeeConsumerApplication.class, args);
+		
+		ConsumerControllerClient consumerControllerClient=ctx.getBean(ConsumerControllerClient.class);
+		System.out.println(consumerControllerClient);
+		consumerControllerClient.getEmployee();
+		
+	}
+	
+	@Bean
+	public  ConsumerControllerClient  consumerControllerClient()
+	{
+		return  new ConsumerControllerClient();
+	}
+}
